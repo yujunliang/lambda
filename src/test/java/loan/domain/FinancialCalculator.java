b6c2f3dfa896056c887dca3eea3d0d41c1e045db
Yujun Liang <yujunliang@hotmail.com>
2010-08-22 13:43:05 -0500

    Yujun - Added comments.

--- src/test/java/loan/domain/FinancialCalculator.java
+++ src/test/java/loan/domain/FinancialCalculator.java
@@ -1,9 +1,19 @@
 package loan.domain;
 
 public class FinancialCalculator {
-    public static double monthlyPayment(double rate, int term, double principle) {
+
+    /**
+     * This formula is from
+     *      http://www.kbapps.com/finance.html
+     *      Simple Interest Amortized Loan Formula
+     * @param rate
+     * @param term
+     * @param principal
+     * @return
+     */
+    public static double monthlyPayment(double rate, int term, double principal) {
         double b = rate / 100;
         double v = Math.pow(b + 1, term);
-        return principle * v / (v - 1) * b;
+        return principal * v / (v - 1) * b;
     }
 }
