ba9865372b7ffd79165f6b11d898b877e80e4d1e
Yujun Liang <yujunliang@hotmail.com>
2015-09-09 09:20:50 -0500

    Added @Ignore AT

--- src/test/java/cucumber/RunCalculationTest.java
+++ src/test/java/cucumber/RunCalculationTest.java
@@ -1,8 +1,10 @@
 package cucumber;
 
 import cucumber.api.junit.Cucumber;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
+@Ignore
 @RunWith(Cucumber.class)
 public class RunCalculationTest {
 }
